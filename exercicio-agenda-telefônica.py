16a7f0bf3fd67c9ed251efb34a3db7ebe92eaf1f
lab135 <>
2023-08-30 20:12:04 -0300

    Exclui um dos contatos presentes na lista 'agenda'

--- exercicio-agenda-telefônica.py
+++ exercicio-agenda-telefônica.py
@@ -22,11 +22,17 @@ while (sair==False):
             for p in agenda:
              print(p)
         if (opcao==3):#Modifica o último contato presente na agenda
-                nome=input("\nDigite o nome do contato a ser editado: ")
-                for  i in range(len(agenda)):
-                    if (agenda[i][0]==nome):
-                        novo_nome=input("Digite o novo nome do contato:")
-                        novo_numero=input("Digite o novo número do contato: ")
-                        agenda[i][0]=novo_nome
-                        agenda[i][1]=novo_numero
-                        os.system('cls')
+            nome=input("\nDigite o nome do contato a ser editado: ")
+            for  i in range(len(agenda)):
+                if (agenda[i][0]==nome):
+                    novo_nome=input("Digite o novo nome do contato:")
+                    novo_numero=input("Digite o novo número do contato: ")
+                    agenda[i][0]=novo_nome
+                    agenda[i][1]=novo_numero
+                    os.system('cls')
+        if(opcao==4):#Remove o último contato criado
+         nome=input("\nDigite o nome do contato a ser editado: ")
+         for  i in range(len(agenda)):
+            if (agenda[i][0]==nome):
+                agenda[i].pop(0)
+                os.system('cls')
