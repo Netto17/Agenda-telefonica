32c1280d915f98adde224763f00525b61b1d7cf7
lab135 <>
2023-08-30 20:09:22 -0300

    Lista os contatos presentes na lista 'agenda'

--- exercicio-agenda-telefônica.py
+++ exercicio-agenda-telefônica.py
@@ -17,22 +17,7 @@ while (sair==False):
         novo_contato.append(numero)
         agenda.append(novo_contato)
         os.system('cls')
-    if(opcao==2):#Lista os contatos presentes na agenda
-        os.system('cls')
-        for p in agenda:
-            print(p)
-    if (opcao==3):#Modifica o último contato presente na agenda
-        nome=input("\nDigite o nome do contato a ser editado: ")
-        for  i in range(len(agenda)):
-          if (agenda[i][0]==nome):
-            novo_nome=input("Digite o novo nome do contato:")
-            novo_numero=input("Digite o novo número do contato: ")
-            agenda[i][0]=novo_nome
-            agenda[i][1]=novo_numero
+        if(opcao==2):#Lista os contatos presentes na agenda
             os.system('cls')
-    if(opcao==4):#Remove o último contato criado
-         nome=input("\nDigite o nome do contato a ser editado: ")
-         for  i in range(len(agenda)):
-            if (agenda[i][0]==nome):
-                agenda[i].pop(0)
-                os.system('cls')
+            for p in agenda:
+             print(p)
